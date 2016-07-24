c2cfdb81042744c1cd9e5ae88bf9a23cc60a496f
Roland Tanglao <roland@rolandtanglao.com>
2016-07-24 07:58:35 -0700

    piechart progress

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # 2016-r-rtgram
 R stuff for instagram vancouver 2016
+## July 23, 2016
+
+1. ```Rscript ig-van-january2016-piechart-as-barchart.R``` # output ig-van-january-2016-piechart-as-barchart.png
+1. ```Rscript ig-van-january2016-piechart.R``` #output ig-van-january-2016-barchart.png BUG in layout of small multiples
---
+++ ig-van-january2016-piechart.R
@@ -0,0 +1,17 @@
+# inspired by 2nd R program in http://www.r-chart.com/2010/07/pie-charts-in-ggplot2.html
+library(ggplot2)
+data3 = read.csv(
+  file="https://raw.githubusercontent.com/rtanglao/2016-rtgram/master/THUMBNAIL_150x150/2016-january-2016-avgcolor-id-mf-month-day-daynum.csv", 
+    stringsAsFactors=F)
+p = ggplot(data=data3, 
+       aes(x=factor(1),
+           y=colour,
+           fill = I(data3$colour)
+      ),
+) 
+p=p + geom_bar(stat="identity", width = 1)
+p=p+facet_grid(facets=. ~ daynumber)
+p = p + coord_polar(theta="y")
+ p = p + xlab('') +
+ylab('')
+ggsave("ig-van-january-2016-barchart.png", p, width = 26.666666667*2, height = 26.666666667*2, dpi = 72, limitsize = FALSE) # 26.6666667 = 1920/72dpi
