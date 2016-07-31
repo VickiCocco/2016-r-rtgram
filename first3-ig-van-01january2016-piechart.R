4aed130951c44ff85fd92a1bc589ce2687811218
Roland Tanglao <roland@rolandtanglao.com>
2016-07-31 05:33:37 -0700

    get real pie chart

--- first3-ig-van-01january2016-piechart.R
+++ first3-ig-van-01january2016-piechart.R
@@ -3,10 +3,10 @@ data3 = read.csv(
   file="01january2016-1st3datapoints.csv", 
     stringsAsFactors=F)
 p = ggplot(data=data3, 
-       aes(x=colour, fill=I(colour)
+       aes(x=factor(1), fill=I(colour)
       ),
            )
-p= p + geom_bar(stat="count")
+p= p + geom_bar(stat="count", width=1)
 p = p + coord_polar(theta="y", start=0)
 
 ggsave("1st3-ig-van-01january-2016-piechart.png", p, width = 26.666666667, height = 26.666666667, dpi = 72, limitsize = FALSE) # 26.6666667 = 1920/72dpi
