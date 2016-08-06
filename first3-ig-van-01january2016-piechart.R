6c47d9dbadb3c7bb25be966168cb8efb000f0dcc
Roland Tanglao <roland@rolandtanglao.com>
2016-08-06 12:28:56 -0700

    add theme void

--- first3-ig-van-01january2016-piechart.R
+++ first3-ig-van-01january2016-piechart.R
@@ -8,6 +8,6 @@ p = ggplot(data=data3,
            )
 p= p + geom_bar(stat="count", width=1)
 p = p + coord_polar(theta="y", start=0)
-
+p = p + theme_void()
 ggsave("1st3-ig-van-01january-2016-piechart.png", p, width = 26.666666667, height = 26.666666667, dpi = 72, limitsize = FALSE) # 26.6666667 = 1920/72dpi
 warnings()
