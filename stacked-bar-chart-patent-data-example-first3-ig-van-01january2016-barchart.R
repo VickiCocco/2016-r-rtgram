f400a61dc1fea0d25a9792e4621ae0109fd82409
Roland Tanglao <roland@rolandtanglao.com>
2016-08-06 09:10:27 -0700

    add example from pie and coxcom plots http://poldham.github.io/ggplot_pizza_patents_part2j/

---
+++ stacked-bar-chart-patent-data-example-first3-ig-van-01january2016-barchart.R
@@ -0,0 +1,11 @@
+library(ggplot2)
+data3 = read.csv(
+  file="01january2016-1st3datapoints.csv", 
+    stringsAsFactors=F)
+p = ggplot(data=data3, 
+       aes(x=1, y=colour, fill=I(colour)
+      ),
+           ) + geom_bar(stat="identity")
+
+ggsave("stacked-bar-chart-patent-data-example-1st3-ig-van-01january-2016-barchart.png", p, width = 26.666666667, height = 26.666666667, dpi = 72, limitsize = FALSE) # 26.6666667 = 1920/72dpi
+warnings()
