4154b248d23843e21f842a483b28e8bc265b15ed
Roland Tanglao <roland@rolandtanglao.com>
2016-09-05 20:41:00 -0700

    add testing script

---
+++ JANUARY2016/test-29january2016.R
@@ -0,0 +1,4 @@
+#debug(waffle) # suppose I want to debug 'getopt' function in 'myscript.R'
+system("Rscript ../file-numphotos-square-piechart.R 29-january2016-ig-van-avgcolour-id-mf-month-day-daynum-unixtime-hour.csv 3101")
+# Debug process start after this. 
+# Check ?browser for help about navigating inside browser
